b781bb0584cbd1a9a7f0c2a287fc5309e1138116
Sowmya Kanithi <kanithisowmya2005@gmail.com>
2025-05-27 12:59:09 +0530

    Create Hyperparameter Tuning.py

---
+++ Titanic Validation/Hyperparameter Tuning.py
@@ -0,0 +1,16 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+clf = RandomForestClassifier()
+param_grid = [
+    {
+        "n_estimators": [10, 100, 200, 500],
+        "max_depth": [None, 5, 10],
+        "min_samples_split": [2, 3, 4],
+    }
+]
+grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=3, scoring="accuracy", return_train_score=True)
+grid_search.fit(X_data, y_data)
+# To view the best parameters:
+print("Best Parameters:", grid_search.best_params_)
+# To view best score:
+print("Best Accuracy:", grid_search.best_score_)
