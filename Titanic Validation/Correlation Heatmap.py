d5bdb6206f0faa2d6e35bc4110703b883b1376db
Sowmya Kanithi <kanithisowmya2005@gmail.com>
2025-05-27 12:35:25 +0530

    Create Correlation Heatmap.py

---
+++ Titanic Validation/Correlation Heatmap.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+titanic_data = sns.load_dataset('titanic')
+numeric_data = titanic_data.select_dtypes(include=['float64', 'int64'])
+
+# Compute correlation matrix
+corr = numeric_data.corr()
+
+# Plot heatmap
+sns.heatmap(corr, cmap='viridis', annot=True)
+plt.show()
