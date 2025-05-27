b7680ac62bb6c02147c5f9063e1ad3d67e4e06c5
Sowmya Kanithi <kanithisowmya2005@gmail.com>
2025-05-27 12:57:23 +0530

    Create categoricals.py

---
+++ Titanic Validation/categoricals.py
@@ -0,0 +1,9 @@
+# Check columns types to identify categoricals
+print(strat_train_set.dtypes)
+X = strat_train_set.drop(['survived'], axis=1)
+y = strat_train_set['survived']
+# Convert categorical columns to numeric with one-hot encoding
+X_encoded = pd.get_dummies(X)
+scaler = StandardScaler()
+X_data = scaler.fit_transform(X_encoded)
+y_data = y.to_numpy()
