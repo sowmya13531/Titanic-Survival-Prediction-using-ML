714601ed17e7606d4e9a9f8a8250f491610476ce
Sowmya Kanithi <kanithisowmya2005@gmail.com>
2025-05-27 12:51:03 +0530

    Create Pipeline.py

---
+++ Titanic Validation/Pipeline.py
@@ -0,0 +1,4 @@
+from sklearn.pipeline import Pipeline
+pipeline = Pipeline([("ageimputer", AgeImputer()),
+                     ("featureencoder", FeatureEncoder()),
+                     ("featuredropper", FeatureDropper())])
